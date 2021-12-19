7e724caaa51c1b7d0c3b85324a0d2d13e343f5b0
Alex Plutta <alex.plutta@googlemail.com>
2021-12-20 00:49:48 +0100

    linitng

--- src/components/TokenDetails/GasChart.jsx
+++ src/components/TokenDetails/GasChart.jsx
@@ -129,6 +129,7 @@ export const GasChart = ({pools}) => {
             new Chart(ctx, {...config, data: {...data, labels},})
         }
         load();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
 
